439ec47386706a47d3318d671e165182f7b315b1
CarolineClark <carolinejaneclark@gmail.com>
2017-09-15 23:31:32 +0100

    Player has trigger collider that NPC detects

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,4 @@
+public class Constants {
+    public static string PLAYER_TAG = "Player";
+    public static string NPC_TAG = "NPC";
+}
---
+++ Assets/Scripts/NPCTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider collider) {
+		Debug.Log("hit");
+		if (collider.tag == Constants.PLAYER_TAG) {
+			Debug.Log("test");
+		}
+	}
+}
