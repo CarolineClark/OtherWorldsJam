974b5e6cfe62d8fd8dee641423459439d1e50720
CarolineClark <carolinejaneclark@gmail.com>
2017-09-15 22:53:47 +0100

    Added crosshair

---
+++ Assets/CrosshairController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrosshairController : MonoBehaviour {
+
+	public float speed = 1;
+	CharacterController characterController;
+	void Start () {
+		characterController = GetComponent<CharacterController>();
+	}
+	
+	void Update () {
+		float horizontal = Input.GetAxis("HorizontalController") * speed;
+		float vertical = Input.GetAxis("VerticalController") * speed;
+		characterController.Move(new Vector3(horizontal, vertical, 0));
+	}
+}
