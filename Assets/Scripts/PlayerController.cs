3f451f77a4ca2124dd460a03d2e3cebb0bf2ef29
CarolineClark <carolinejaneclark@gmail.com>
2017-09-16 12:19:50 +0100

    Player stationary and walk animation done

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,14 +6,18 @@ public class PlayerController : MonoBehaviour {
 
 	public float speed = 1;
 	CharacterController characterController;
+	Animator animator;
 	void Start () {
 		characterController = GetComponent<CharacterController>();
+		animator = GetComponent<Animator>();
 	}
 	
 	void Update () {
 		float horizontal = Input.GetAxis(Constants.PLAYER_HORIZONTAL_INPUT) * speed;
 		float vertical = Input.GetAxis(Constants.PLAYER_VERTICAL_INPUT) * speed;
-		characterController.Move(new Vector3(horizontal, vertical, 0));
+		Vector3 velocity = new Vector3(horizontal, vertical, 0);
+		animator.SetBool("move", (velocity.magnitude > 0));
+		characterController.Move(velocity);
 	}
 
 	public void Kill() {
