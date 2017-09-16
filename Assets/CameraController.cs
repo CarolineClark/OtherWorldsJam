b36d5cea97348e136c4af41e72b4b05c5a3de93e
CarolineClark <carolinejaneclark@gmail.com>
2017-09-16 12:27:19 +0100

    Added CameraController

---
+++ Assets/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	GameObject player;
+	Vector3 offset = new Vector3(0, 0, -1);
+
+	void Start () {
+		player = GameObject.FindGameObjectWithTag(Constants.PLAYER_TAG);
+	}
+	
+	void Update () {
+		transform.position = player.transform.position + offset;
+	}
+}
