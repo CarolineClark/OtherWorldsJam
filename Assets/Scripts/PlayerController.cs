8c027bee26b416655c267c81fd50b228aa9a9937
dittto <nick@ditt.to>
2017-09-16 12:14:58 +0100

    Adds rotation to player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
 	public float speed = 1;
+    public float rotationSpeed = 1;
+
 	CharacterController characterController;
 	void Start () {
 		characterController = GetComponent<CharacterController>();
@@ -14,7 +16,20 @@ public class PlayerController : MonoBehaviour {
 		float horizontal = Input.GetAxis(Constants.PLAYER_HORIZONTAL_INPUT) * speed;
 		float vertical = Input.GetAxis(Constants.PLAYER_VERTICAL_INPUT) * speed;
 		characterController.Move(new Vector3(horizontal, vertical, 0));
-	}
+
+        transform.rotation = Quaternion.LookRotation(
+            Vector3.RotateTowards(
+                transform.forward,
+                new Vector3(horizontal, vertical, 0),
+                rotationSpeed * Time.deltaTime,
+                0f
+            ), Vector3.forward
+        );
+
+        Vector3 eulerAngles = transform.rotation.eulerAngles;
+        eulerAngles = new Vector3(0, 0, eulerAngles.z);
+        transform.rotation = Quaternion.Euler(eulerAngles);
+    }
 
 	public void Kill() {
 		EventManager.TriggerEvent(Constants.EVENT_PLAYER_DIE);
